64295e75a4e732b64ed727f34b36d6e2945c5d59
Novikov Eugene <evgeniy.novikov@neuro-city.ru>
2023-09-13 17:25:41 +0300

    Редактирование статьи. Подготовка редактора. Функционал сохранения.

---
+++ src/bff/operations/save-post.js
@@ -0,0 +1,23 @@
+import { sessions } from '../sessions';
+import { ROLE } from '../constants';
+import { updatePost } from '../api';
+
+export const savePost = async (userSessionHash, newPostData) => {
+	const accessRoles = [ROLE.ADMIN];
+
+	const access = await sessions.access(userSessionHash, accessRoles);
+
+	if (!access) {
+		return {
+			error: 'Доступ запрещён',
+			res: null,
+		};
+	}
+
+	const updatedPost = await updatePost(newPostData);
+
+	return {
+		error: null,
+		res: updatedPost,
+	};
+};
--- src/pages/post/components/post-content/post-content.js
+++ src/pages/post/components/post-content/post-content.js
@@ -1,35 +1,27 @@
-import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { H2, Icon } from '../../../../components';
+import { SpecialPanel } from '../special-panel/special-panel';
+import styled from 'styled-components';
 
-const PostContentContainer = ({ className, post: { title, imageUrl, publishedAt, content } }) => {
+const PostContentContainer = ({ className, post: { id, title, imageUrl, publishedAt, content } }) => {
+	const navigate = useNavigate();
 
 	return (
 		<div className={className}>
 			<img src={imageUrl} alt={title} />
 			<H2>{title}</H2>
-			<div className='special-panel'>
-				<div className='published-at'>
-					<Icon
-						id='fa-calendar-o'
-						size='18px'
-						margin='0 7px 0 0'
-					/>
-					{publishedAt}
-				</div>
-				<div className='buttons'>
+			<SpecialPanel
+				publishedAt={publishedAt}
+				margin='-20px 0 20 px'
+				editButton={
 					<Icon
 						id='fa-pencil-square-o'
 						size='21px'
 						margin='0 10px 0 0'
-						onClick={() => {}}
+						onClick={() => navigate(`/post/${id}/edit`)}
 					/>
-					<Icon
-						id='fa-trash-o'
-						size='21px'
-						onClick={() => {}}
-					/>
-				</div>
-			</div>
+				}
+			/>
 			<div className='post-text'>{content}</div>
 		</div>
 	);
@@ -41,29 +33,8 @@ export const PostContent = styled(PostContentContainer)`
 		margin: 0 20px 10px 0;
 	}
 
-	& .special-panel {
-		display: flex;
-		justify-content: space-between;
-		margin: -20px 0 20px;
-	}
-
-	& .published-at {
-		display: flex;
-		font-size: 18px;
-	}
-
-	& i {
-		position: relative;
-		top: -1px;
-	}
-
-	& .buttons {
-		display: flex;
-		position: relative;
-		top: -2px;
-	}
-
 	& .post-text {
 		font-size: 18px;
+		white-space: pre-line;
 	}
 `;
