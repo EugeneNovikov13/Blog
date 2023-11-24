c5f8a171ad547a5e52cc8372479dade93b5a473d
EugeneNovikov13 <eugene.work.4940@gmail.com>
2023-11-24 20:34:39 +0300

    Пользователи: логин, логаут, регистрация, изменение списка пользователей админом

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { register } = require('./controllers/user');
+const { login, register, getUsers, getRoles, updateUser, deleteUser } = require('./controllers/user');
 const mapUser = require('./helpers/mapUser');
+const authenticated = require('./middlewares/authenticated');
+const hasRole = require('./middlewares/hasRole');
+const ROLES = require('./constants/roles');
 
 const port = 3001;
 const app = express();
@@ -12,13 +15,58 @@ app.use(express.json());
 
 app.post('/register', async (req, res) => {
 	try {
-		const user = await register(req.body.login, req.body.password)
+		const { user, token } = await register(req.body.login, req.body.password);
 
-		res.send({error: null, user: mapUser(user)})
+		res.cookie('token', token, { httpOnly: true })
+			.send({ error: null, user: mapUser(user) });
 	} catch (e){
 		res.send({error: e.message || "Unknown message"})
 	}
 })
+
+app.post('/login', async (req, res) => {
+	try {
+		const { user, token } = await login(req.body.login, req.body.password);
+
+		res.cookie('token', token, { httpOnly: true })
+			.send({ error: null, user: mapUser(user) });
+	} catch (e) {
+		res.send({ error: e.message || 'Unknown message' });
+	}
+});
+
+app.post('/logout', (req, res) => {
+	res.cookie('token', '', { httpOnly: true })
+		.send({});
+});
+
+app.use(authenticated);
+
+app.get('/users', hasRole([ROLES.ADMIN]), async (req, res) => {
+	const users = await getUsers();
+
+	res.send({ data: users.map(mapUser) });
+});
+
+app.get('/users/roles', hasRole([ROLES.ADMIN]), (req, res) => {
+	const roles = getRoles();
+
+	res.send({ data: roles });
+});
+
+app.patch('/users/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+	await updateUser(req.params.id, {
+		role: req.body.roleId,
+	});
+
+	res.send({ data: mapUser(newUser) });
+});
+
+app.delete('/users/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+	await deleteUser(req.params.id);
+
+	res.send({ error: null });
+});
 
 mongoose.connect(
 	'mongodb+srv://NovikovEugene:gfhjkm13@educationdb.nioilpj.mongodb.net/blog?retryWrites=true&w=majority',
